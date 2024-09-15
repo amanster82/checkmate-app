43ea2fa280ae81526e380de186a016742c314de3
Aman Bhayani <amanbhayani@hotmail.com>
2024-09-15 00:32:29 -0700

    Update front-end: 1. Seperated Member file from dialog 2. Upload with progress bars on Memberfile 3. Various states to display

--- web/vml-app/app/App.tsx
+++ web/vml-app/app/App.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-import { BlueprintProvider, Button, Card, Classes, ControlGroup, Dialog, DialogBody, DialogFooter, Elevation, FileInput, FormGroup, Icon, InputGroup, Label } from "@blueprintjs/core";
+import { Alignment, BlueprintProvider, Button, Card, Classes, ControlGroup, Dialog, DialogBody, DialogFooter, Elevation, EntityTitle, FileInput, FormGroup, Icon, InputGroup, Label, Menu, MenuDivider, MenuItem, Navbar } from "@blueprintjs/core";
 import './events.css'
 
 function app() {
@@ -13,6 +13,40 @@ function app() {
     const [imagePreview, setImagePreview] = useState(null);
     const [fileA, setFileA] = useState([]);
     const [fileB, setFileB] = useState([]);
+    const [hasMembershipFile, setHasMembershipFile] = useState(false);
+    const [showMemberDialog, setMemberDialog] = useState(false);
+    const [uploadProgress, setUploadProgress] = useState(0);
+    const [eventsData, setEventsData] = useState([]);
+
+    React.useEffect(() => {
+        const setUpState = async () => {
+            const memberFileExits = await axios.get('http://localhost:8080/api/files/membershipExists');
+            console.log("membershipFile", memberFileExits);
+            if (memberFileExits.data === "Does not exist!") {
+                setHasMembershipFile(false);
+            } else {
+                setHasMembershipFile(memberFileExits.data);
+                setFileB(['', memberFileExits.data]);
+            }
+
+        }
+
+        setUpState();
+
+    }, [])
+
+
+    React.useEffect(() => {
+
+        const getAllEvents = async () => {
+            const response = await axios.get('http://localhost:8080/api/v1/events/getAllEvents');
+            console.log(response.data);
+            setEventsData(response.data);
+        }
+
+        getAllEvents()
+
+    }, [])
 
 
     const handleSubmit = async (event: any) => {
@@ -26,53 +60,76 @@ function app() {
         if (fileData.entries().next().value[1] !== null) {
             const response = await axios.post('http://localhost:8080/api/files/upload', fileData, {
                 headers: {
-                  "Content-Type": "multipart/form-data",
+                    "Content-Type": "multipart/form-data",
                 },
-                onUploadProgress:progressEvent => {
+                onUploadProgress: progressEvent => {
                     console.log("Uploading : " + ((progressEvent.loaded / progressEvent.total) * 100).toString() + "%")
                 }
             });
         }
 
-        const url = 'http://localhost:8080/api/v1/event';
-        let formData = {...forminput, 
-                        'guestListFile': fileA,
-                        'membershipFile': fileB,
-                        'coverImage': imageData
-                    }
+        const url = 'http://localhost:8080/api/v1/events';
+        let formData = {
+            ...forminput,
+            'guestListFile': fileA,
+            'membershipFile': fileB,
+            'coverImage': imageData
+        }
 
         console.log(formData);
 
         const config = {
-          headers: {
-            'Accept': 'application/json',
-            'content-type': 'multipart/form-data',
-          },
+            headers: {
+                'Accept': 'application/json',
+                'content-type': 'multipart/form-data',
+            },
         };
-        
+
         let x = await axios.post(url, forminput)
         console.log(x.data);
 
 
     }
-    
+
+    const uploadMemberFile = async (event: any) => {
+        event.preventDefault()
+        const fileData = new FormData();
+        fileData.append('title', 'Membership');
+        fileData.append('files', fileB[0]);
+        if (fileData.entries().next().value[1] !== null) {
+            const response = await axios.post('http://localhost:8080/api/files/upload', fileData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                onUploadProgress: progressEvent => {
+                    console.log("Uploading : " + ((progressEvent.loaded / progressEvent.total) * 100).toString() + "%")
+                    setUploadProgress(((progressEvent.loaded / progressEvent.total) * 100))
+                }
+            });
+            if (response.status == 200) {
+                console.log("response from upload:", response)
+                setHasMembershipFile(response.data);
+            }
+        }
+    }
+
     const handleFileUpload = async (event: any, type: string) => {
         const originalFile = event.target.files[0];
-    
+
         // Modify the file name based on the type
         let modifiedFileName = originalFile.name;
         if (type === "A") {
             modifiedFileName = "A_" + originalFile.name;
         } else if (type === "B") {
-            modifiedFileName = "B_" + originalFile.name;
+            modifiedFileName = originalFile.name;
         } else {
             modifiedFileName = "img_" + originalFile.name;
             setImagePreview(URL.createObjectURL(originalFile)); // Set the image preview if it's an image
         }
-    
+
         // Create a new File object with the modified name
         const modifiedFile = new File([originalFile], modifiedFileName, { type: originalFile.type });
-    
+
         // Update the state based on the type
         if (type === "A") {
             setFileA([modifiedFile, originalFile.name]);
@@ -83,38 +140,117 @@ function app() {
         }
     };
 
-    return (
-        <>
-            <Card interactive={false} elevation={Elevation.TWO}>
-                <div className='event-card'>
-                    <h1>My Party</h1>
+    const handleCloseDialog = () => {
+        setMemberDialog(false);
+        setUploadProgress(0);
+    }
+
+    const returnEvents = () => {
+        
+        return eventsData.map((event) => (
+
+            <Card interactive={true} elevation={Elevation.TWO} className='event-card'>
+                <div className='event-header'>
+                    <h1>{event.title}</h1>
                 </div>
                 <div style={{ textAlign: 'right' }}>
-                    <Button text="Enter" className={Classes.BUTTON} />
-                    <Button text="Settings" className={Classes.BUTTON} onClick={() => setIsOpen(true)} />
+                    <Button icon="cog" text="Settings" className={Classes.BUTTON} onClick={() => setIsOpen(true)} />
                 </div>
-
             </Card>
+            
+        ));
+            
+    }
 
-            <Card className="create-event" interactive={true} elevation={Elevation.TWO} onClick={() => setIsOpen(true)}>
+
+    return (
+        <>
+
+            <Navbar>
+                <Navbar.Group align={Alignment.LEFT}>
+                    <Navbar.Heading>CheckMate</Navbar.Heading>
+                    <Navbar.Divider />
+                    <Button onClick={() => setMemberDialog(true)} className="bp5-minimal" icon={hasMembershipFile ? "document" : "error"} text={hasMembershipFile ? hasMembershipFile : "Membership File Missing"} />
+                </Navbar.Group>
+            </Navbar>
+            
+            {returnEvents()}
+
+            <Card className="create-event" interactive={hasMembershipFile} elevation={Elevation.TWO} onClick={() => setIsOpen(true)}>
                 <h1>Create an Event</h1>
-                <Icon icon="plus" size={20} />
+
+                {!hasMembershipFile &&
+
+                    <div className='flex'>
+                        <EntityTitle icon={<Icon icon="cross-circle" color='red' title=""></Icon>} title='Please upload membership file before creating an event' />
+                    </div>
+                }
+
+                {hasMembershipFile && <Icon icon="plus" size={20} />}
             </Card>
+
+
+            <Dialog title="Membership File" icon="info-sign" isOpen={showMemberDialog}>
+                <DialogBody>
+
+                    <ControlGroup fill={false} vertical={true}>
+                        <FormGroup
+                            helperText="Must be in CSV format"
+                            label="Member List File"
+                            labelFor="member-list"
+                            labelInfo="(required)"
+                        >
+                            <FileInput id="member-list" disabled={false} text={fileB[1] ? fileB[1] : "Choose file..."} onChange={(e) => handleFileUpload(e, "B")} />
+                        </FormGroup>
+                    </ControlGroup>
+
+                    {uploadProgress !== 0 && uploadProgress !== 100 &&
+                        <>
+
+                            <div className="bp5-progress-bar bp5-intent-primary {{.modifier}}">
+                                <div className="bp5-progress-meter" style={{ width: uploadProgress + '%' }}></div>
+                            </div>
+
+                        </>
+
+                    }
+
+                    {uploadProgress === 100 &&
+                        <>
+
+                            <div className='flex'>
+                                <EntityTitle icon={<Icon icon="tick-circle" color='green' title="Successfully Uploaded"></Icon>} title='Succesfully Uploaded' />
+                            </div>
+
+                        </>
+
+                    }
+
+
+
+
+                </DialogBody>
+                <DialogFooter actions={
+                    <>
+                        <Button intent="none" text="Close" onClick={handleCloseDialog} />
+                        <Button intent="primary" text="Submit" onClick={uploadMemberFile} />
+                    </>} />
+            </Dialog>
 
             <Dialog title="Create an Event" icon="info-sign" isOpen={isOpen}>
                 <DialogBody>
                     <ControlGroup fill={false} vertical={true}>
                         <Label htmlFor="input-b">Event Title</Label>
-                        <input onChange={(e)=>setFormInput({...forminput, title: e.target.value})} className={Classes.INPUT} id="input-b" placeholder="Ex. Selena and Marks Wedding" />
+                        <input onChange={(e) => setFormInput({ ...forminput, title: e.target.value })} className={Classes.INPUT} id="input-b" placeholder="Ex. Selena and Marks Wedding" />
                         <Label htmlFor="input-b">Event Date</Label>
-                        <input onChange={(e)=>setFormInput({...forminput, eventDate: e.target.value })} className={Classes.INPUT} id="input-b" placeholder="Placeholder text" type="date" />
+                        <input onChange={(e) => setFormInput({ ...forminput, eventDate: e.target.value })} className={Classes.INPUT} id="input-b" placeholder="Placeholder text" type="date" />
                         <FormGroup
                             helperText="Can be an Event Brite order file, must be in CSV format"
                             label="Guest List File"
                             labelFor="guest-list"
                             labelInfo="(required)"
                         >
-                            <FileInput id="guest-list" disabled={false} text={fileA[1] ? fileA[1] : "Choose file..."} onChange={(e)=> handleFileUpload(e, "A")}/>
+                            <FileInput id="guest-list" disabled={false} text={fileA[1] ? fileA[1] : "Choose file..."} onChange={(e) => handleFileUpload(e, "A")} />
 
                         </FormGroup>
 
@@ -124,7 +260,7 @@ function app() {
                             labelFor="member-list"
                             labelInfo="(required)"
                         >
-                            <FileInput id="member-list" disabled={false} text={fileB[1] ? fileB[1] : "Choose file..."} onChange={(e)=> handleFileUpload(e, "B")} />
+                            <FileInput id="member-list" disabled={false} text={fileB[1] ? fileB[1] : "Choose file..."} onChange={(e) => handleFileUpload(e, "B")} />
 
                         </FormGroup>
 
@@ -133,13 +269,13 @@ function app() {
                             labelFor="cover-image"
                             labelInfo="(optional)"
                         >
-                            <FileInput id="cover-image" disabled={false} text={imageData[1] ? imageData[1] : "Choose file..."}  onChange={(e)=>handleFileUpload(e, "IMAGE")} />
+                            <FileInput id="cover-image" disabled={false} text={imageData[1] ? imageData[1] : "Choose file..."} onChange={(e) => handleFileUpload(e, "IMAGE")} />
 
                         </FormGroup>
 
                     </ControlGroup>
-                    
-                    <img src={imagePreview ? imagePreview : undefined} height="100" width="250"/>
+
+                    <img src={imagePreview ? imagePreview : undefined} height="100" width="250" />
 
                 </DialogBody>
                 <DialogFooter actions={
